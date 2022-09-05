dc5ef33b68b4ceffe3e21d818b5741b1b6d4bbc3
Marine BD <marine.bap-deletang@aum.bio>
2022-09-05 12:07:43 +0200

    flake8 ignore type of errors

--- oc_lettings_site/settings/__init__.py
+++ oc_lettings_site/settings/__init__.py
@@ -5,6 +5,12 @@ env_name = os.getenv("ENV_NAME", "local")
 print("ENV_NAME", env_name)
 
 if env_name == "heroku":
-    from .heroku import *
+    from .heroku import DEBUG, ALLOWED_HOSTS, SECRET_KEY
 else:
-    from .local import *
+    from .local import DEBUG, ALLOWED_HOSTS, SECRET_KEY
+
+    # FALKE 8 IGNORE IMPORT * !!!!!!!!!!!
+# --> Check deplooy config.yml
+# --> "heroku env" is nowhere
+# --> nettoyer, clarifier, commenter
+# ---> deploy on heroku CHECK DOC
