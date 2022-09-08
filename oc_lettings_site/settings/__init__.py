869d3a9cdddcf56583c0dc2a729646ed2b43ad93
Marine BD <marine.bap-deletang@aum.bio>
2022-09-08 18:27:23 +0200

    replace code in init settings

--- oc_lettings_site/settings/__init__.py
+++ oc_lettings_site/settings/__init__.py
@@ -1,13 +1,13 @@
-# from .base import *
+from .base import *
 
-# env_name = os.getenv("ENV_NAME", "local")
+env_name = os.getenv("ENV_NAME", "heroku")
 
-# print("ENV_NAME", env_name)
+print("ENV_NAME", env_name)
 
-# if env_name == "heroku":
-#     from .heroku import DEBUG, ALLOWED_HOSTS, SECRET_KEY
-# else:
-#     from .local import DEBUG, ALLOWED_HOSTS, SECRET_KEY
+if env_name == "heroku":
+    from .heroku import DEBUG, ALLOWED_HOSTS, SECRET_KEY
+else:
+    from .local import DEBUG, ALLOWED_HOSTS, SECRET_KEY
 
 # FALKE 8 IGNORE IMPORT * !!!!!!!!!!!
 # --> Check deplooy config.yml
