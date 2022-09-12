4f090868b301dc5ce07f276f22ea8852dff49c64
Marine BD <marine.bap-deletang@aum.bio>
2022-09-12 16:17:06 +0200

    replace sqlite bd in root

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -103,16 +103,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = "/static/"
 
-sentry_sdk.init(
-    dsn=f"os.environ.get('SENTRY')",
-    integrations=[
-        DjangoIntegration(),
-    ],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+if "SENTRY_DSN" in os.environ:
+    sentry_sdk.init(
+        dsn=f"os.environ.get('SENTRY_DSN')",
+        integrations=[
+            DjangoIntegration(),
+        ],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
