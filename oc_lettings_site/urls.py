c086fc4a9ef2f4418bb5e9043f31444493c5f575
Marine BD <marine.bap-deletang@aum.bio>
2022-09-13 12:05:16 +0200

    cleaning

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,7 @@ from profiles import views as p_views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("lettings/", l_views.index, name="lettings_index"),  # ANCHOR change name ?
+    path("lettings/", l_views.index, name="lettings_index"),
     path("lettings/<int:letting_id>/", l_views.letting, name="letting"),
     path("profiles/", p_views.index, name="profiles_index"),
     path("profiles/<str:username>/", p_views.profile, name="profile"),
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -5,8 +5,6 @@ def index(request):
     return render(request, "index.html")
 
 
-from django.urls import path
-
-
 def trigger_error(request):
     division_by_zero = 1 / 0
+    return division_by_zero
